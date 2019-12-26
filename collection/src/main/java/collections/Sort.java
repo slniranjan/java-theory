f5c5e21f61e72717819b8c00bef40a5903984669
Niranjan Thilakarathna <nirandhammika@gmail.com>
2019-12-26 13:38:12 +0530

    remove unused imports

--- collection/src/main/java/collections/Sort.java
+++ collection/src/main/java/collections/Sort.java
@@ -2,10 +2,7 @@ package collections;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 public class Sort {
 
